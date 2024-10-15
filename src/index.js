e69985259564e33aab5f2bc7d8d07d38e86b9aec
Subham Burman <subhamburman05@gmail.com>
2024-10-15 13:56:57 +0530

    create server

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    return res.send('This is HOME page');
+
+})
+app.post('/post', (req,res)=>{
+    return res.send('using post request');
+
+})
+app.put('/put', (req,res)=>{
+    return res.send('using put method');
+
+})
+app.delete('/delete', (req,res)=>{
+    return res.send('deleting sonthing...');
+
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on PORT : ${PORT}`);
+
+})
